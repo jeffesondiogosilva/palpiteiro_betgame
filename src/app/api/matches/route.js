3d0d615b42cf154264ca63d09635edeb24f43135
jeff <diogoworkss@gmail.com>
2025-02-11 22:38:57 -0300

    fix: corrigindo busca de jogos

--- src/app/api/matches/route.js
+++ src/app/api/matches/route.js
@@ -5,22 +5,23 @@ const prisma = new PrismaClient();
 
 export async function GET(req) {
     try {
-        const todayString = new Date().toISOString().split("T")[0]; // "2025-02-01"
-        const startOfDay = new Date(todayString); // "2025-02-01T00:00:00.000Z"
+
+        const todayString = new Date().toISOString().split("T")[0]; // "2025-02-11"
+        const startOfDay = new Date(todayString); // "2025-02-11T00:00:00.000Z"
         const endOfDay = new Date(todayString);
         endOfDay.setUTCHours(23, 59, 59, 999);
-
+        
         const matches = await prisma.match.findMany({
             where: {
                 date: {
-                    gte: startOfDay,
-                    lte: endOfDay,
-                },
-            },
-            orderBy: { date: "asc" },
+                    gte: startOfDay, // Maior ou igual a meia-noite de hoje
+                    lte: endOfDay    // Menor ou igual a 23:59:59 de hoje
+                }
+            }
         });
-
-
+        
+        console.log(matches);
+        
         return new Response(JSON.stringify(matches), {
             headers: { "Content-Type": "application/json" },
         });
